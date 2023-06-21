e50d06befa6e7065415b4c9cbd48373a0a5801aa
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-21 07:53:54 -0400

    before pwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,5 @@ export default function Home() {
     console.log(file);
   };
 
-  return (
-    <div className="border-2">
-      {/* <MyAppbar /> */}
-      {/* <NavbarAuth /> */}
-      {/* <div className="z-10 fixed bottom-0 left-0 right-0 flex justify-center items-center w-full h-20 bg-gray-100 dark:bg-gray-900 dark:text-gray-100 text-gray-900 text-sm font-medium">
-        <FixedBottomNavigation />
-      </div> */}
-      {/* <Suspense fallback={<div>Loading...</div>}>
-        <ImageUploader onFileSelect={handleFileSelect} />
-      </Suspense> */}
-    </div>
-  );
+  return <div className="border-2"></div>;
 }
