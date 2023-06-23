47042f04b3ca7d459f10811b9923869277bbd42e
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-22 21:10:04 -0400

    removing supabase

--- src/app/(bottomNav)/dashboard/page.tsx
+++ src/app/(bottomNav)/dashboard/page.tsx
@@ -1,25 +1,10 @@
 import { FC } from "react";
-import isLoggedInServer from "@/app/lib/isLoggedInServer";
 import { redirect } from "next/navigation";
 import DashboardGridUi from "@/app/components/DashboardGridUi";
-import { createServerClient } from "@/app/lib/supabase-server";
 
 interface pageProps {}
 
 const page: FC<pageProps> = async () => {
-  if ((await isLoggedInServer()) !== true) {
-    console.log("not logged in on dashboard");
-    redirect("/");
-  }
-  // const supabase = createServerClient();
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
-  // if (!session) {
-  //   redirect("/");
-  // }
-  // console.log(session);
-
   return (
     <div className="grid grid-cols-1 justify-start items-center w-full">
       <div className="text-2xl text-center mb-4 w-full">Dashboard</div>
