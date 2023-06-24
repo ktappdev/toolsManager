22424ff1acbb2e3661767ebdeb5cbaf6584b7271
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-24 02:02:57 -0400

    enabled after auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,30 +10,30 @@ import { NextResponse } from "next/server";
 const whiteList = ["ktad592@gmail.com", "kendabeatmaker@gmail.com"]; //"kendabeatmaker@gmail.com"
 export default authMiddleware({
   publicRoutes: ["/", "/sign-in", "/sign-up", "/not-authorized"],
-  // afterAuth(auth, req) {
-  //   if (
-  //     auth.sessionClaims?.email &&
-  //     req.nextUrl.pathname === "/not-authorized" &&
-  //     whiteList.includes(auth.sessionClaims.email.toString())
-  //   ) {
-  //     const dashboard = new URL("/dashboard", req.url);
-  //     return NextResponse.redirect(dashboard);
-  //   }
+  afterAuth(auth, req) {
+    if (
+      auth.sessionClaims?.email &&
+      req.nextUrl.pathname === "/not-authorized" &&
+      whiteList.includes(auth.sessionClaims.email.toString())
+    ) {
+      const dashboard = new URL("/dashboard", req.url);
+      return NextResponse.redirect(dashboard);
+    }
 
-  //   if (!auth.userId && !auth.isPublicRoute) {
-  //     return redirectToSignIn({ returnBackUrl: req.url });
-  //   }
-  //   // console.log(auth);
-  //   if (
-  //     auth.sessionClaims?.email &&
-  //     !whiteList.includes(auth.sessionClaims.email.toString()) &&
-  //     !auth.isPublicRoute
-  //   ) {
-  //     console.log("I can implement my own whitelist here using clerk");
-  //     const notAuthorized = new URL("/not-authorized", req.url);
-  //     return NextResponse.redirect(notAuthorized);
-  //   }
-  // },
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // console.log(auth);
+    if (
+      auth.sessionClaims?.email &&
+      !whiteList.includes(auth.sessionClaims.email.toString()) &&
+      !auth.isPublicRoute
+    ) {
+      console.log("I can implement my own whitelist here using clerk");
+      const notAuthorized = new URL("/not-authorized", req.url);
+      return NextResponse.redirect(notAuthorized);
+    }
+  },
 });
 
 // export default authMiddleware();
