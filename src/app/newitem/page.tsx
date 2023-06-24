d3cabe833a9086a658f6fe4c3fa38c9357c0117e
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-24 08:06:35 -0400

    remove axios

--- src/app/newitem/page.tsx
+++ src/app/newitem/page.tsx
@@ -1,18 +1,10 @@
 import { FC } from "react";
-import CameraComponent from "../components/CameraComponent";
-import fetchPost from "@/app/lib/fetchPost";
-import { prisma } from "@/app/lib/prismaClient";
 import NewItemForm from "../components/NewItemForm";
 interface pageProps {}
 
-const page: FC<pageProps> = async ({}) => {
+export default async function page() {
   // const res = await prisma.car.findMany();
   // console.log(res);
-
-  const handleFileSelect = (file: File) => {
-    // Do something with the selected file
-    console.log(file);
-  };
 
   return (
     <div className="flex flex-col items-center justify-center h-full w-3/4 lg:w-full mt-4 overflow-auto">
@@ -20,6 +12,4 @@ const page: FC<pageProps> = async ({}) => {
       <NewItemForm />
     </div>
   );
-};
-
-export default page;
+}
