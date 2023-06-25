f9ea91be0933870732b9bfb909db34723c0bdbe5
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-25 08:32:22 -0400

    welcome page sign in logic update

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,17 +1,28 @@
 "use client";
+import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
 const LoginButton = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
   const router = useRouter();
   return (
-    <button
-      onClick={() => {
-        router.push("/sign-in");
-      }}
-      className="px-4 py-2 bg-c-accent-1 text-white rounded hover:bg-blue-700"
-    >
-      Login / Sign-up
-    </button>
+    <>
+      {!isSignedIn && isLoaded && (
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-sm  mb-6">
+            Login if you have an account or signup
+          </p>
+          <button
+            onClick={() => {
+              router.push("/sign-in");
+            }}
+            className="px-4 py-2 bg-c-accent-1 text-white rounded hover:bg-blue-700"
+          >
+            Login / Sign-up
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
