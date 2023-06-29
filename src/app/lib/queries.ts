562c9a2f89cd47eec030b81efacff729f953e75c
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-29 08:09:59 -0400

    added iTool interface

--- src/app/lib/queries.ts
+++ src/app/lib/queries.ts
@@ -1,8 +1,10 @@
 "use server";
 import { prisma } from "./prismaClient";
+import { iTool } from "./interfaces";
 
-export const getTools = async () => {
-  const tools = await prisma.tools.findMany();
+export const getTools = async (): Promise<iTool[]> => {
+  const tools: iTool[] = await prisma.tools.findMany();
   console.log(tools);
   return tools;
 };
+
